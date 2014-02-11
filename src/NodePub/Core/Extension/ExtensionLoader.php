b8eb52f5d6ea5b39016c034de63f7939a62cc54f
Andrew Gruner <andrew.gruner@gmail.com>
2014-02-11 11:27:10 -0800

    using NodePub\Core\Trait\SourceDirectoryAware interface and trait

--- src/NodePub/Core/Extension/ExtensionLoader.php
+++ src/NodePub/Core/Extension/ExtensionLoader.php
@@ -2,27 +2,17 @@
 
 namespace NodePub\Core\Extension;
 
+use NodePub\Common\Trait\SourceDirectoryAwareInterface;
+use NodePub\Common\Trait\SourceDirectoryAwareTrait;
 use Symfony\Component\Finder\Finder;
 
-class ExtensionLoader
+class ExtensionLoader implements SourceDirectoryAwareInterface
 {
-    protected $sourceDirs = array();
+    use SourceDirectoryAwareTrait;
     
     function __construct()
     {
         $this->addSource(__DIR__ . '/../Extensions');
-    }
-    
-    /**
-     * Adds a directory path to the list of sources to load from.
-     */
-    public function addSource($sourcePath)
-    {
-        if (is_dir($sourcePath) || is_link($sourcePath)) {
-            $this->sourceDirs[] = $sourcePath;
-        } else {
-            throw new \Exception("Specified load path is not a directory");
-        }
     }
     
     /**
@@ -33,17 +23,12 @@ class ExtensionLoader
      */
     protected function findExtensionDirectories()
     {
-        $extDirs = Finder::create()
+        $finder = Finder::create()
             ->directories()
             ->depth('== 0')
             ->name('*Extension')
             ;
         
-        # add all source paths to the finder
-        foreach ($this->sourceDirs as $dir) {
-           $extDirs->in($dir);
-        }
-        
-        return $extDirs;
+        return $this->findInSourceDirs($finder);
     }
 }
